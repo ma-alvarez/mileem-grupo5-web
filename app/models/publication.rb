87ca3ff2a798996702f7a72f63a5261c39d85436
ma-alvarez <magual89@gmail.com>
2014-09-14 22:23:31 -0300

    validaciones al modelo de publicaciones

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,6 @@
 class Publication < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :transaction_type, :address, :phone, :price, presence: true
+  validates_numericality_of :number_of_rooms, :price, :age, :expenses, :area, greater_than:0
 end
