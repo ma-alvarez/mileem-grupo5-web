b1aa8fc308f5e983ee78d3d06276b2caa101cfb0
ma-alvarez <magual89@gmail.com>
2014-09-19 18:30:10 -0300

    refactor api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,10 +4,6 @@ class ApiController < ApplicationController
 
   def all_publications
   	@publications = Publication.all
-  	  @publications.each do |p|
-      @images = PublicationAttachment.where(publication_id: p.id)
-      p.publication_attachments = @images
-    end
     respond_with @publications.as_json(include: {publication_attachments: {only: :image}})
   end
 
