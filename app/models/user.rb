9cf73c1eb573313288a39cd9ea4e912a49fdba03
ma-alvarez <magual89@gmail.com>
2014-09-20 00:40:16 -0300

    id:260 state:done time:1 comment:registracion basica de usuarios lista permite tipo de cuenta

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :publications 
+
+  ACCOUNT_TYPES = [['Free',"Free"],['Basic','Basic'],['Premium','Basic']]
 end
