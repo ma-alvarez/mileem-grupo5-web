bff86c10837867d06a7da82c664ff9789ca8f9e5
ma-alvarez <magual89@gmail.com>
2014-09-27 13:39:06 -0300

    id:403, id:405, id:406, id:408 state:in testing time:0.2 comment:se agregan migraciones para cambiar tipo de dato en la bd y validaciones en el modelo

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,7 +3,7 @@ class Publication < ActiveRecord::Base
   has_many :publication_attachments
   accepts_nested_attributes_for :publication_attachments
   validates :transaction_type, :address, :phone, :price, presence: true
-  validates_numericality_of :number_of_rooms, :price, :age, :expenses, :area, greater_than:0
+  validates_numericality_of :number_of_rooms, :price, :age, :expenses, :area, greater_than:0, only_integer:true
   validate :publication_date_cannot_be_in_the_past
 
   TRANSACTION_TYPES = [['Compra','Compra'],['Alquiler','Alquiler']]
