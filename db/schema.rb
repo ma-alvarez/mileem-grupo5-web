881a5f7a4e468415002cc0fcb4c33ac0aae5dbc9
ma-alvarez <magual89@gmail.com>
2014-10-03 20:53:17 -0300

    id:450, id:451 state:in testing time:1

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20140927162947) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "full_name"
-    t.string   "account_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
