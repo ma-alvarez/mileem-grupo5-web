0d46b09999d0a64fde447bec9ab5d5835998f79e
ma-alvarez <magual89@gmail.com>
2014-10-10 19:04:47 -0300

    id:438 state:in testing time:0.2

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -5,6 +5,7 @@ class Publication < ActiveRecord::Base
 
   validates :transaction_type, :address, :zone, :price, presence: true
   validates_numericality_of :number_of_rooms, :price, :age, :expenses, :area, greater_than_or_equal_to:0, only_integer:true
+  validates_numericality_of :price, less_than_or_equal_to:1000000000
   validate :publication_date_cannot_be_in_the_past
 
   TRANSACTION_TYPES = [['Compra','Compra'],['Alquiler','Alquiler']]
