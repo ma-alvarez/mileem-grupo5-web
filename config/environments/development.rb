c667e229183c30611de16db12debefc0ba9379f9
MartinOGE <martin.gorgazzi@gmail.com>
2014-10-13 20:10:20 -0300

    Confirmacion de correo en las cuentas de usuario.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Raise an exception message if there is an error send an email
+  config.action_mailer.raise_delivery_errors = true
+  
+  # Email delivery method
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "mileem.grupo5",
+    password: "mileemgrupo5"
+  }
+  
 end
