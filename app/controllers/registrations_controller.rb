59d14932e5316e7d6a4a4dabdb0f63bfe20e5770
ma-alvarez <magual89@gmail.com>
2014-11-13 17:51:38 -0300

    id:806 time:3 state:done, id:807 time:3 state:done, id:808 state:done time:1

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+  def destroy
+    if current_user.destroy
+      binding.pry
+      current_user.publications.each do |publication|
+         publication.retire
+         publication.save
+      end
+      redirect_to root_url, notice: "El usuario ha sido eliminado."
+    end
+  end
+end
